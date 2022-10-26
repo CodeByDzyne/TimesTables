92230f8515b13d0006d8f14dcf03bcf781800431
CodeByDzyne <wdenley@dzynesolutions.com>
2022-10-26 19:10:25 +1100

    Initial Commit

---
+++ TimesTables/TimesTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimesTablesApp.swift
+//  TimesTables
+//
+//  Created by Wealthy Waz on 26/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct TimesTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
